a2a6883847f5c80588062e4b459af9bda83fe7f6
William Guo <billy.r.guo@gmail.com>
2017-06-12 15:00:55 -0500

    added code to test zipline accuracy
    Added some lines of code to faber.py to export the performance dataframe returned after back testing, which is then used to get the individual transactions per trading day.

--- algos/faber.py
+++ algos/faber.py
@@ -2,11 +2,12 @@ import sys
 sys.path.append('C:/Users/William/Documents/GitHub/faber-trading-strategy/algos')
 from numpy import mean
 from collections import defaultdict
-from zipline.api import order, record, symbol, date_rules, time_rules, schedule_function
+from zipline.api import order, order_target, record, symbol, date_rules, time_rules, schedule_function
 from write_to_sql import run
 import pandas as pd
 import os
-from zipline import run_algorithm
+
+from zipline.finance import commission
 
 def initialize(context):
     """
@@ -25,16 +26,25 @@ def initialize(context):
                       symbol('XLP'), 
                       symbol('XLY')]
 
+    # context.benchmark = symbol('GOOG')
+
+    # context.symbol = [symbol('AAPL'),
+    #                   symbol('MSFT')]
+
     # skip the first 300 days of the timeframe so that we have enough data to calculate our 10 month SMA
     context.skip = 0
+    context.init = True
 
     # keep track of number of shares bought
     context.shares = defaultdict(int)
     context.moving_avg = defaultdict(int)
-    context.monthly_price = defaultdict(list)
+    context.monthly_price = defaultdict(list)  
+    context.closing_price = defaultdict(int)
+
+    context.set_commission(commission.PerShare(cost=0))
 
     # skip the first 10 months so that we have enough data to establish our moving average    
-    schedule_function(buy_monthly, date_rules.month_end(), time_rules.market_close())
+    schedule_function(buy_monthly, date_rules.month_end(days_offset=0), time_rules.market_close(minutes=2))
 
 def handle_data(context, data):
     """
@@ -55,55 +65,61 @@ def buy_monthly(context, data):
     
     Output: some kind of action (buy/sell/nothing) on the last trading day of each month
     """
-    # context.portfolio.starting_cash = float(100000)
 
     context.skip += 1
-    money_spent = 0
+    value = context.portfolio.portfolio_value
+
+    # reevaluate portfolio
+    context.positions = 0
+    for asset in context.symbol:
+        context.positions += context.shares[asset] * data.current(asset, 'close')
 
     if context.skip < 10:
         for asset in context.symbol:
-            price = data.current(asset, 'price')
+            price = data.current(asset, 'close')
             context.monthly_price[asset].append(price)
 
     else:
         # record benchmark's "first" price
-        if context.skip == 10:
-            context.ratio = context.portfolio.portfolio_value / data.current(context.benchmark, 'price')
+        if context.init == True:
+            context.ratio = context.portfolio.portfolio_value / data.current(context.benchmark, 'close')
+            context.init = False
 
         for asset in context.symbol:
-            price = data.current(asset, 'price')
+            price = data.current(asset, 'close')
 
             # Get closing price on last trading day of month
             context.monthly_price[asset].append(price)
 
+            if context.skip > 10:
+                del context.monthly_price[asset][0]
+
             # calculate the 10-month moving average of each asset
-            context.moving_avg[asset] = mean(context.monthly_price[asset][context.skip - 9: context.skip])
+            context.moving_avg[asset] = mean(context.monthly_price[asset])
         
         ### Faber's trading strategy ###
         
-        # if the current price exceeds moving average, long
+        # if the current price exceeds moving average and we haven't already bought any shares, buy
         for asset in context.symbol:
             # the most current monthly price will be the one added most recently (so it'll be the element on the end of the list)
-            if context.monthly_price[asset][-1] > context.moving_avg[asset] and money_spent < context.portfolio.portfolio_value:
-                order(asset, 500)
-                context.shares[asset] += 500
+            if context.monthly_price[asset][-1] >= context.moving_avg[asset] and context.shares[asset] == 0:
+                # order_target(asset, 50, limit_price=data.current(asset, 'price'))
+                order_target(asset, 50)
+                context.shares[asset] = 50
 
-                # add amount ordered to total money spent during this specific buying period
-                money_spent += 500 * data.current(asset, 'price')
-
-            # else if the current price is below moving average, short
-            elif context.monthly_price[asset][-1] < context.moving_avg[asset]:
-                order(asset, -context.shares[asset])
+            # else if the current price is below moving average and we have 500 shares of the asset, sell
+            elif context.monthly_price[asset][-1] < context.moving_avg[asset] and context.shares[asset] == 50:
+                order_target(asset, 0)
                 context.shares[asset] = 0
-
-            # save/record the data for future plotting
-            # record(asset = context.monthly_price[asset][-1], sma = context.moving_avg[asset])
 
         # record portfolio value
         record(portfolio = context.portfolio.portfolio_value)
 
+        # record returns
+        record(returns = context.portfolio.returns)
+
         # also record the S&P 500 monthly price
-        record(SPY = context.ratio * data.current(context.benchmark, 'price'))
+        record(SPY = context.ratio * data.current(context.benchmark, 'close'))
      
 def analyze(context = None, results = None):
     """
@@ -115,14 +131,28 @@ def analyze(context = None, results = None):
     """
     import matplotlib.pyplot as plt
 
+    txn = results['transactions']
+    txn.to_csv('transactions.csv')
+
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
 
     # plot both the portfolio based on faber's strategy and a buy-and-hold strategy
-    results.portfolio_value.plot(ax=ax1)
+    results['portfolio'].plot(ax=ax1)
     results['SPY'].plot(ax=ax1)
-
     ax1.set_ylabel('Portfolio value (USD)')
+
+    ax2 = fig.add_subplot(212)
+    results['returns'].plot(ax=ax2)
+    ax2.set_ylabel('Cumulative Returns')
+
+    # export portfolio values to csv file
+    # results['portfolio'].to_csv('zipline2.csv')
+
+    results['SPY'].to_csv('benchmark.csv')
+   
+    # export portfolio values to csv file
+    results['returns'].to_csv('zipline_returns.csv')
 
     plt.show()
 
